4ce9fdcf74a6381fa60c2290f864af2fa80275f4
Renaud <38732257+renaudjester@users.noreply.github.com>
2025-04-22 10:04:53 +0200

    Update tests/test_deprecations.py
    Co-authored-by: Oriol Ricart Vilarrubias. <45894267+uriii3@users.noreply.github.com>

--- tests/test_deprecations.py
+++ tests/test_deprecations.py
@@ -1,5 +1,5 @@
 URL_METADATA = "https://stac.marine.copernicus.eu/metadata/INSITU_ARC_PHYBGCWAV_DISCRETE_MYNRT_013_031/cmems_obs-ins_arc_phybgcwav_mynrt_na_irr_202311--ext--history/dataset.stac.json"  # noqa
 
 
-class TestDepecations:
+class TestDeprecations:
     pass
