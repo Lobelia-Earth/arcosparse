623e3c4b748f9e0a3224d451a2149e4b0db3f99c
Renaud <38732257+renaudjester@users.noreply.github.com>
2025-04-22 10:13:00 +0200

    fix: handle 0 coordinates (#34)
    * fix: do not test
    
    * Update tests/test_deprecations.py
    
    Co-authored-by: Oriol Ricart Vilarrubias. <45894267+uriii3@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Oriol Ricart Vilarrubias. <45894267+uriii3@users.noreply.github.com>

--- tests/test_deprecations.py
+++ tests/test_deprecations.py
@@ -1,13 +1,5 @@
-from arcosparse import get_platforms_names
-
 URL_METADATA = "https://stac.marine.copernicus.eu/metadata/INSITU_ARC_PHYBGCWAV_DISCRETE_MYNRT_013_031/cmems_obs-ins_arc_phybgcwav_mynrt_na_irr_202311--ext--history/dataset.stac.json"  # noqa
 
 
-class TestDepecations:
-
-    def test_get_platforms_names_deprecated(self, caplog):
-        result = get_platforms_names(URL_METADATA)
-        assert "WARNING" in caplog.text
-        assert "'get_platforms_names' is deprecated" in caplog.text
-        assert result
-        assert "F-Vartdalsfjorden___MO" in result
+class TestDeprecations:
+    pass
