5a4ca2dfa23300ecca52f972e9f59524dd9d0d71
Renaud <38732257+renaudjester@users.noreply.github.com>
2026-02-12 16:11:39 +0100

    feat: release pandas constraint (#43)
    * feat: release pandas constraint
    
    * fix: update lock file
    
    * test: update test without ecmwf dataset

--- tests/test_get_entities.py
+++ tests/test_get_entities.py
@@ -3,12 +3,12 @@ from dataclasses import asdict
 import arcosparse
 
 URL_METADATA_1 = "https://stac.marine.copernicus.eu/metadata/INSITU_ARC_PHYBGCWAV_DISCRETE_MYNRT_013_031/cmems_obs-ins_arc_phybgcwav_mynrt_na_irr_202311--ext--history/dataset.stac.json"  # noqa
-URL_METADATA_2 = "https://object-store.os-api.cci2.ecmwf.int/cadl-metadata/metadata/satellite_lake_water_level/multi-track/dataset.stac.json"  # noqa
+# URL_METADATA_2 = "https://object-store.os-api.cci2.ecmwf.int/cadl-metadata/metadata/satellite_lake_water_level/multi-track/dataset.stac.json"  # noqa
 
 
 class TestGetEntities:
     def test_get_entities(self, snapshot):
-        for i, url in enumerate([URL_METADATA_1, URL_METADATA_2], start=1):
+        for i, url in enumerate([URL_METADATA_1], start=1):
             entities = arcosparse.get_entities(url)
             assert entities is not None
             assert len(entities) > 0
